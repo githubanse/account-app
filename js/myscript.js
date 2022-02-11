62fba1b4d9f117fb5516f5808a0b6877f1cb20e2
leonbonou <leonbonou20@gmail.com>
2022-02-11 14:39:13 +0100

    first commit11241

--- js/myscript.js
+++ js/myscript.js
@@ -5,9 +5,10 @@ const btn = document.getElementById('btn_Increment');
 const showPour = document.getElementById('showPour');
 showPour.innerText = pourcentage;
 
-function Increment() {
+function Increment(e) {
     if (pourcentage === 99) {
-        window.location.href ='fail.html'
+        window.location.href = 'fail.html'
+        e.preventDefault();
     } else {
         pourcentage = pourcentage + 1;
     }

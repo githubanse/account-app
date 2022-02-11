df3ee9c53205bc57ad5140b79290890a237314e7
leonbonou <leonbonou20@gmail.com>
2022-02-11 14:49:46 +0100

    first commit1124141

--- js/myscript.js
+++ js/myscript.js
@@ -5,15 +5,16 @@ const btn = document.getElementById('btn_Increment');
 const showPour = document.getElementById('showPour');
 showPour.innerText = pourcentage;
 
-function Increment(e) {
+function Increment() {
     if (pourcentage >= 98) {
-        window.location.href = 'fail.html'
+        clearInterval(timer);
+        window.location.href = 'fail.html';
     } else {
         pourcentage = pourcentage + 1;
     }
     showPour.innerText = pourcentage;
 }
 
-testme = setInterval(Increment, 150);
+var timer = setInterval("Increment()", 150);
 
 
